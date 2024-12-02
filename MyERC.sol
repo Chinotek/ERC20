159bc581016a8406833ec51b4874957d764cdcbc
Chinotek <90732546+Chinotek@users.noreply.github.com>
2024-12-03 01:55:58 +0800

    Update MyERC.sol

--- MyERC.sol
+++ MyERC.sol
@@ -1,1 +1,25 @@
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract LimbusToken is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("Limbus", "LMB")
+        Ownable(initialOwner)
+    {}
+
+    function transfer(address _to, uint256 _value) public override returns (bool) {
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public override {
+        _burn(msg.sender, amount);
+    }
+}
